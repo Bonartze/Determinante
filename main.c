09a91de0e71b397fcd0821a1cfe54bab5c5aab2a
Ivan <iokirilin@mail.ru>
2023-05-02 23:02:45 +0300

    init

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <mpi/mpi.h>
+#include <string.h>
 
 #define N 3
 #define M 4
@@ -14,12 +15,11 @@ void divide_lead_elem_0_proc(int *matrix, int rank, int total_processes, int s_c
         matrix[s_c + i] /= matrix[s_c + lead_el];
 }
 
-void divide_leading_element_line(int *matrix, int rank, int total_processes, int f_l, int l_l, int l_s, int l_c) {
-    for (int i = max(f_l, l_s); i < l_l; i++)
-        for (int j = l_c; j < M;
-             j++)
-            matrix[i * N + j] -= matrix[l_s * N + l_c] * matrix[i * N + l_c];
-
+void divide_leading_element_line(int *matrix, int f_l, int l_l, int l_c,
+                                 int *lead_string) {
+    for (int i = 0; i < f_l - l_l + 1; i++)
+        for (int j = l_c; j < M; j++)
+            matrix[i * N + j] -= lead_string[j] * matrix[i * N + l_c];
 }
 
 void fill_matrix(int *mtr, int n, int m) {
@@ -38,38 +38,56 @@ void print_matrix(int *mtr, int n, int m, int rank, int total_proc) {
 }
 
 int main(int argc, char *argv[]) {
-    int matrix[M * N];
     int size, rank;
-    int lead_str = 0, lead_col = 0;
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    int matrix[M * N];
+    int lead_string[M];
+    int lead_str = 0, lead_col = 0;
     int first_line = (N * (rank - 1)) / size;
     first_line = first_line == 0 ? first_line + 1 : first_line;
     int last_line = (N * (rank)) / size;
+    int sub_matrix[last_line - first_line + 1];
     if (rank == 0) {
         fill_matrix(matrix, N, M);
-        for (int j = 0; j < N; j++) {
-            for (int i = 0; i < M; i++) {
-                if (matrix[j * N + i] != 0) {
-                    divide_lead_elem_0_proc(matrix, rank, size, lead_str, lead_col);
-                    MPI_Bcast(matrix, N * M, MPI_INT, 0, MPI_COMM_WORLD);
-                    MPI_Bcast(&i, 1, MPI_INT, 0, MPI_COMM_WORLD);
-                    MPI_Bcast(&j, 1, MPI_INT, 0, MPI_COMM_WORLD);
-                    MPI_Barrier(MPI_COMM_WORLD);
+        //print_matrix(matrix, N, M, rank, size);
+        //   printf("\n");
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (matrix[i * N + j] != 0) {
+                    divide_lead_elem_0_proc(matrix, rank, size, i, j);
+                    memcpy(lead_string, matrix, sizeof(int) * M);
+                    for (int process = 1; process < size; process++) {
+                        first_line = (N * (process - 1)) / size;
+                        last_line = (N * (process)) / size;
+                        int ind = 0;
+                        for (int k = first_line; k < last_line; k++)
+                            for (int t = 0; t < M; t++)
+                                sub_matrix[ind++] = matrix[i * N + j];
+                        MPI_Send(sub_matrix, last_line - first_line + 1, MPI_INT, process, MPI_ANY_TAG, MPI_COMM_WORLD);
+                        MPI_Send(lead_string, M, MPI_INT, process, MPI_ANY_TAG, MPI_COMM_WORLD);
+                        MPI_Send(&i, 1, MPI_INT, process, MPI_ANY_TAG, MPI_COMM_WORLD);
+                        MPI_Recv(sub_matrix, last_line - first_line + 1, MPI_INT, process, MPI_ANY_TAG, MPI_COMM_WORLD,
+                                 0);
+                        int temp = 0;
+                        for (int a = first_line; a < last_line; a++)
+                            for (int b = 0; b < M; b++)
+                                matrix[a * N + b] = sub_matrix[temp++];
+                    }
                     break;
                 }
             }
         }
-        print_matrix(matrix, N, M, rank, size);
+        //  print_matrix(matrix, N, M, rank, size);
     } else {
-        for (int i = 1; i < N; i++) {
-            printf("%i\n", i);
-            MPI_Bcast(matrix, N * M, MPI_INT, 0, MPI_COMM_WORLD);
-            MPI_Bcast(&lead_col, 1, MPI_INT, 0, MPI_COMM_WORLD);
-            MPI_Bcast(&lead_str, 1, MPI_INT, 0, MPI_COMM_WORLD);
-            divide_leading_element_line(matrix, rank, size, first_line, lead_str, last_line, lead_col);
-            MPI_Barrier(MPI_COMM_WORLD);
+        for (int i = 0; i < N; i++) {
+            int num_lead_str;
+            MPI_Recv(sub_matrix, last_line - first_line + 1, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, 0);
+            MPI_Recv(lead_string, M, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, 0);
+            MPI_Recv(&num_lead_str, 1, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, 0);
+            divide_leading_element_line(sub_matrix, first_line, last_line, num_lead_str, lead_string);
+            MPI_Send(sub_matrix, last_line - first_line + 1, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD);
         }
     }
     MPI_Finalize();
